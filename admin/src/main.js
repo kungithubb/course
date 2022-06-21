096ca501462beb1d369322f8f17e2361d94ca5cf
liukun <kunliu01@163.com>
2022-06-21 09:27:55 +0800

    生成枚举

--- admin/src/main.js
+++ admin/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './app.vue'
 import router from './router'
 import axios from 'axios'
+import filter from './filter/filter'
 
 Vue.config.productionTip = false
 Vue.prototype.$ajax = axios;
@@ -22,5 +23,10 @@ new Vue({
   router,
   render: h => h(App),
 }).$mount('#app')
+
+// 全局过滤器
+Object.keys(filter).forEach(key => {
+  Vue.filter(key, filter[key])
+});
 
 console.log("环境：", process.env.NODE_ENV);
--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -37,7 +37,7 @@
               <td>{{section.chapterId}}</td>
               <td>{{section.video}}</td>
               <td>{{section.time}}</td>
-              <td>{{section.charge}}</td>
+              <td>{{SECTION_CHARGE | optionKV(section.charge)}}</td>
               <td>{{section.sort}}</td>
             <td>
               <div class="hidden-sm hidden-xs btn-group">
@@ -97,7 +97,9 @@
                 <div class="form-group">
                   <label class="col-sm-2 control-label">收费</label>
                   <div class="col-sm-10">
-                    <input v-model="section.charge" class="form-control">
+                    <select v-model="section.charge" class="form-control">
+                      <option v-for="o in SECTION_CHARGE" v-bind:value="o.key">{{o.value}}</option>
+                    </select>
                   </div>
                 </div>
                 <div class="form-group">
@@ -126,7 +128,8 @@
     data: function() {
       return {
         section: {},
-        sections: []
+        sections: [],
+        SECTION_CHARGE: SECTION_CHARGE,
       }
     },
     mounted: function () {
